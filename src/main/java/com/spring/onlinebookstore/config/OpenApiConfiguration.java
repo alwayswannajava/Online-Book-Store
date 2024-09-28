bb0ae53270dfaf9d5c91d3d9d10209e56e8c6c10
JaverJaver <mykhailo.kornukh@gmail.com>
2024-09-28 13:07:52 +0300

    fix some issues

--- src/main/java/com/spring/onlinebookstore/config/OpenApiConfiguration.java
+++ src/main/java/com/spring/onlinebookstore/config/OpenApiConfiguration.java
@@ -5,26 +5,42 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:open-api.properties")
 public class OpenApiConfiguration {
+    private static final String OPEN_API_TITLE = "Online Book Store API";
+    private static final String OPEN_API_VERSION = "1.0";
+    private static final String OPEN_API_DESCRIPTION = "This API exposes endpoints "
+            + "to manage books.";
+
+    @Value("${open.api.url}")
+    private String openApiUrl;
+    @Value("${open.api.description}")
+    private String description;
+    @Value("${open.api.author.credentials}")
+    private String credentials;
+    @Value("${open.api.author.email}")
+    private String email;
 
     @Bean
     public OpenAPI defineOpenApiInfo() {
         Server server = new Server();
-        server.setUrl("http://localhost:8080");
-        server.setDescription("Development");
+        server.setUrl(openApiUrl);
+        server.setDescription(description);
 
         Contact myContact = new Contact();
-        myContact.setName("Mykhailo Kornukh");
-        myContact.setEmail("mykhailo.kornukh@gmail.com");
+        myContact.setName(credentials);
+        myContact.setEmail(email);
 
         Info information = new Info()
-                .title("Online Book Store API")
-                .version("1.0")
-                .description("This API exposes endpoints to manage books.")
+                .title(OPEN_API_TITLE)
+                .version(OPEN_API_VERSION)
+                .description(OPEN_API_DESCRIPTION)
                 .contact(myContact);
         return new OpenAPI().info(information).servers(List.of(server));
     }
