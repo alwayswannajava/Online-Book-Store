740e7c01632f16519a8a6311006ebbe8dde6bc17
JaverJaver <mykhailo.kornukh@gmail.com>
2024-11-07 20:01:44 +0200

    fix comments

--- src/main/java/com/spring/onlinebookstore/config/OpenApiConfiguration.java
+++ src/main/java/com/spring/onlinebookstore/config/OpenApiConfiguration.java
@@ -1,7 +1,10 @@
 package com.spring.onlinebookstore.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +19,9 @@ public class OpenApiConfiguration {
     private static final String OPEN_API_VERSION = "1.0";
     private static final String OPEN_API_DESCRIPTION = "This API exposes endpoints "
             + "to manage books.";
+    private static final String SECURITY_AUTH_SCHEME_NAME = "bearerAuth";
+    private static final String SECURITY_SCHEME_NAME = "bearer";
+    private static final String SECURITY_BEARER_FORMAT = "JWT";
 
     @Value("${open.api.url}")
     private String openApiUrl;
@@ -32,6 +38,15 @@ public class OpenApiConfiguration {
                 .title(OPEN_API_TITLE)
                 .version(OPEN_API_VERSION)
                 .description(OPEN_API_DESCRIPTION);
-        return new OpenAPI().info(information).servers(List.of(server));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(SECURITY_AUTH_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_AUTH_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_AUTH_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme(SECURITY_SCHEME_NAME)
+                                .bearerFormat(SECURITY_BEARER_FORMAT)))
+                .info(information).servers(List.of(server));
     }
 }
