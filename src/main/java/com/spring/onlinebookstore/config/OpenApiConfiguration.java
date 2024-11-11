e73c2fc77ff85e05e5b96a465df50ec97cdd881e
JaverJaver <mykhailo.kornukh@gmail.com>
2024-11-12 01:36:50 +0200

    fix comments

--- src/main/java/com/spring/onlinebookstore/config/OpenApiConfiguration.java
+++ src/main/java/com/spring/onlinebookstore/config/OpenApiConfiguration.java
@@ -1,10 +1,8 @@
 package com.spring.onlinebookstore.config;
 
-import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,14 +12,17 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:open-api.properties")
+@io.swagger.v3.oas.annotations.security.SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class OpenApiConfiguration {
     private static final String OPEN_API_TITLE = "Online Book Store API";
     private static final String OPEN_API_VERSION = "1.0";
     private static final String OPEN_API_DESCRIPTION = "This API exposes endpoints "
             + "to manage books.";
-    private static final String SECURITY_AUTH_SCHEME_NAME = "bearerAuth";
-    private static final String SECURITY_SCHEME_NAME = "bearer";
-    private static final String SECURITY_BEARER_FORMAT = "JWT";
 
     @Value("${open.api.url}")
     private String openApiUrl;
@@ -39,14 +40,6 @@ public class OpenApiConfiguration {
                 .version(OPEN_API_VERSION)
                 .description(OPEN_API_DESCRIPTION);
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(SECURITY_AUTH_SCHEME_NAME))
-                .components(new Components()
-                        .addSecuritySchemes(SECURITY_AUTH_SCHEME_NAME, new SecurityScheme()
-                                .name(SECURITY_AUTH_SCHEME_NAME)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme(SECURITY_SCHEME_NAME)
-                                .bearerFormat(SECURITY_BEARER_FORMAT)))
                 .info(information).servers(List.of(server));
     }
 }
--- src/main/java/com/spring/onlinebookstore/config/SecurityConfig.java
+++ src/main/java/com/spring/onlinebookstore/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,11 +34,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/auth/**",
-                                "/swagger-ui/**", "/v3/api-docs/**")
+                                "/swagger-ui/**", "/api-docs/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated())
-                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter,
